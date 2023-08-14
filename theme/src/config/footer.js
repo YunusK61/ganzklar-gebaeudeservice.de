c51c2bbf5ecf2f18d00d5a16bbd758dd13cb5f54
Rami Nakkar <58343300+rami-nk@users.noreply.github.com>
2023-08-14 21:55:29 +0000

    changes

--- theme/src/config/footer.js
+++ theme/src/config/footer.js
@@ -1,80 +1,44 @@
 export const footerSocials = [
-  {
-    name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
-    icon: "mdi:twitter",
-  },
-  {
-    name: 'Youtube',
-    url: 'https://youtube.com/c/LittleSticks',
-    icon: "mdi:youtube",
-  },
-  {
-    name: 'Github',
-    url: 'https://github.com/littlesticks',
-    icon: "mdi:github",
-  },
-  {
-    name: 'Discord',
-    url: 'https://littlesticks.dev/discord',
-    icon: "ic:baseline-discord",
-  },
+ // {
+    // name: 'Instagram',
+    // url: 'https://twitter.com/littlesticksdev',
+    // icon: "mdi:instagram",
+  //}
 ]
 
 export const footerLists = [
 	{
-		title: 'Landing Pages',
+		title: 'Kontakt',
 		items: [
 			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
+				title: "Tylestr. 9, 86156 Augsburg"
 			},
 			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
+				title: "+4913412341242134"
 			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
+			{
+				title: "test@mail.com"
+			}
 		],
 	},
 	{
-		title: 'Company',
 		items: [
 			{
-				title: 'About',
+				title: 'Über uns',
 				slug: '/company/about',
 			},
 			{
-				title: 'Blog',
-				slug: '/blog',
+				title: 'Datenschutz',
+				slug: '/company/legal',
 			},
 			{
-				title: 'Contact',
+				title: 'Impressum',
+				slug: '/company/impressum',
+			},
+			{
+				title: 'Kontakt',
 				slug: '/company/contact',
 			},
-		],
-	},
-	{
-		title: 'Theme',
-		items: [
-			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
-			},
-			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
-			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
 		],
 	},
 ];
